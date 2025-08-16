687b059b49d952457b3ebbbcda481c8e5c4fa81c
makecy <martinstefanoski9@hotmail.com>
2025-08-16 19:27:18 +0200

    implemented Makefile and RPN class sturcture and interface

---
+++ ex01/RPN.hpp
@@ -0,0 +1,41 @@
+#ifndef RPN_HPP
+#define RPN_HPP
+
+#include <stack>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <exception>
+
+class RPN {
+private:
+    std::stack<double> _stack;
+    bool isOperator(const std::string& token) const;
+    bool isNumber(const std::string& token) const;
+    double performOperation(double a, double b, const std::string& op) const;
+    
+public:
+    RPN();
+    ~RPN();
+    double calculate(const std::string& expression);
+    class InvalidExpressionException : public std::exception {
+    public:
+        virtual const char* what() const throw() {
+            return "Error";
+        }
+    };
+    class DivisionByZeroException : public std::exception {
+    public:
+        virtual const char* what() const throw() {
+            return "Error";
+        }
+    };
+    class InvalidTokenException : public std::exception {
+    public:
+        virtual const char* what() const throw() {
+            return "Error";
+        }
+    };
+};
+
+#endif

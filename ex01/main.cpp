56423245564ef74aef69ae1eaac43d4f17e3c4e6
makecy <martinstefanoski9@hotmail.com>
2025-08-17 15:57:12 +0200

    added simple tests with outputs and changed NAME in makefile

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -18,3 +18,21 @@ int main(int argc, char** argv) {
     
     return 0;
 }
+
+// some tests for RPN calc
+
+
+// # Provided examples
+// ./RPN "8 9 * 9 - 9 - 9 - 4 - 1 +"    # Output: 42
+// ./RPN "7 7 * 7 -"                     # Output: 42
+// ./RPN "1 2 * 2 / 2 * 2 4 - +"         # Output: 0
+// ./RPN "(1 + 1)"                       # Output: Error
+
+// # Additional tests
+// ./RPN "1 2 +"                         # Output: 3
+// ./RPN "5 3 -"                         # Output: 2
+// ./RPN "4 2 /"                         # Output: 2
+// ./RPN "3 4 * 2 +"                     # Output: 14
+// ./RPN "1 0 /"                         # Output: Error (division by zero)
+// ./RPN "1 2"                           # Output: Error (too many operands)
+// ./RPN "1 +"                           # Output: Error (insufficient operands)
